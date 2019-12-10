6af854cd3ce8a67f4b18cce045ee63a81c119d41
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-12-09 18:39:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -143,8 +143,9 @@ if(message.content.startsWith(prefix + " start")) {
     });
   });
 })
-  
 };
+
+});
 
 // THE CLIENT LOGIN PROCESS (BOT READS THE TOKEN FROM HERE)
 // ADDITIONALLY YOU COULD ADD YOUR OWNERID INSTEAD 
